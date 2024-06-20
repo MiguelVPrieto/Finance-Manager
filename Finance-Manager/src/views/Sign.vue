0ee464c0cf96bb9d045e80e5672f5ee45cb63beb
MiguelVPrieto <miguelprieto.espanha@gmail.com>
2024-06-20 19:42:42 -0300

    final base for signup

--- Finance-Manager/src/views/Sign.vue
+++ Finance-Manager/src/views/Sign.vue
@@ -24,13 +24,12 @@
             @click:append-inner="visible = !visible"
           ></v-text-field>
           <v-text-field
-            :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="visible ? 'text' : 'password'"
+            :append-inner-icon="visible2 ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="visible2 ? 'text' : 'password'"
             label="Confirm Password"
             :rules="confirmPasswordRules()"
             prepend-icon="mdi-key"
             v-model="confirmPassword"
-            @click:append-inner="visible = !visible"
           ></v-text-field>
 
           <v-btn class="mt-2" type="submit" block>Submit</v-btn>
@@ -46,6 +45,7 @@ export default {
   data: () => {
     return ({
       visible: false,
+      visible2: false,
       password: '',
       confirmPassword: '',
       firstNameRules: [
