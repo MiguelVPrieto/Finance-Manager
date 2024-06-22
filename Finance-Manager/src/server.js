d3e0562c820a9dbf6dfab3ad4f4622407eb10d6e
MiguelVPrieto <miguelprieto.espanha@gmail.com>
2024-06-21 22:18:24 -0300

    data 9

--- Finance-Manager/src/server.js
+++ Finance-Manager/src/server.js
@@ -44,7 +44,7 @@ app.post('/submit-form', (req, res) => {
   });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
