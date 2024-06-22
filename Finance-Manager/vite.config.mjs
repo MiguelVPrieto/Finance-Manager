e6f83dd0d4b1f23ace72d71117e8ad61123dd696
MiguelVPrieto <miguelprieto.espanha@gmail.com>
2024-06-21 22:33:09 -0300

    data 10

--- Finance-Manager/vite.config.mjs
+++ Finance-Manager/vite.config.mjs
@@ -14,7 +14,6 @@ export default defineConfig({
     Vue({
       template: { transformAssetUrls }
     }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
     Vuetify(),
     Components(),
     ViteFonts({
@@ -43,5 +42,12 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-  },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' }
+      }
+    }
+  }
 })
